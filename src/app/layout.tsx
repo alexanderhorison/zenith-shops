aa3b99ef9e1fb477e30d824e037935680aa82e9a
alexbithealth <alexbithealth@gmail.com>
2025-12-25 13:25:51 +0700

    feat: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import { AppToaster } from "@/components/app-toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         {children}
         <AppToaster />
+        <SpeedInsights />
       </body>
     </html>
   );
